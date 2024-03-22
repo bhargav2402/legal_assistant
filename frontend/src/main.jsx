fcdbc9da9cf811c4201482f8d5f4cca1411853d0
manascb1344 <zephop76593@gmail.com>
2024-03-22 21:45:57 +0530

    basic Init

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,16 +2,10 @@ import React from "react";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-// import AppRoutes from "./AppRoutes";
+import AppRoutes from "./AppRoutes";
 import "./index.css";
-// import ContextProvider from "./MainDashboard/contexts/ContextProvider";
-// import { registerLicense } from "@syncfusion/ej2-base";
-// import { Analytics } from "@vercel/analytics/react";
-// import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const AppWrapper = () => {
-	registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE);
-
 	return (
 		<Auth0Provider
 			domain={"dev-5jmtzlok7krwi77v.us.auth0.com"}
@@ -20,9 +14,9 @@ const AppWrapper = () => {
 				redirect_uri: window.location.origin,
 			}}
 		>
-				<Router>
-
-				</Router>
+			<Router>
+				<AppRoutes />
+			</Router>
 		</Auth0Provider>
 	);
 };
