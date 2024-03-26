0940106bbb46807c13b0684d4f19ab318c599681
manascb1344 <zephop76593@gmail.com>
2024-03-26 23:31:42 +0530

    Removed Auth0

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Auth0Provider } from "@auth0/auth0-react";
+// import { Auth0Provider } from "@auth0/auth0-react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import AppRoutes from "./AppRoutes";
@@ -7,17 +7,17 @@ import "./index.css";
 
 const AppWrapper = () => {
 	return (
-		<Auth0Provider
-			domain={"dev-5jmtzlok7krwi77v.us.auth0.com"}
-			clientId={"vzUywLFt1sPUkg5mDeEplCLkbVW7cz83"}
-			authorizationParams={{
-				redirect_uri: window.location.origin,
-			}}
-		>
-			<Router>
-				<AppRoutes />
-			</Router>
-		</Auth0Provider>
+		// <Auth0Provider
+		// 	domain={"dev-5jmtzlok7krwi77v.us.auth0.com"}
+		// 	clientId={"vzUywLFt1sPUkg5mDeEplCLkbVW7cz83"}
+		// 	authorizationParams={{
+		// 		redirect_uri: window.location.origin,
+		// 	}}
+		// >
+		<Router>
+			<AppRoutes />
+		</Router>
+		// </Auth0Provider>
 	);
 };
 
